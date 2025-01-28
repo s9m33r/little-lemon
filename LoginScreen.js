92570e0a97ab97d6b6d8eabbcf93985fc343ce8c
s9m33r <sam33r.driving@gmail.com>
2025-01-28 13:26:59 +0530

    Init

---
+++ LoginScreen.js
@@ -0,0 +1,57 @@
+import { ScrollView, Text, StyleSheet, TextInput } from 'react-native';
+import React from 'react';
+
+export default function LoginScreen() {
+    const [email, onChangeEmail] = React.useState('')
+    const [password, onChangePassword] = React.useState('')
+
+    return (
+        <ScrollView style={styles.container} keyboardDismissMode='on-drag'>
+            <Text style={styles.headerText}>Welcome to Little Lemon</Text>
+            <Text style={styles.regularText}>Login to continue </Text>
+            <TextInput
+                style={styles.inputBox}
+                value={email}
+                onChangeText={onChangeEmail}
+                placeholder={'email'}
+                keyboardType={'email-address'}
+            />
+            <TextInput
+                style={styles.inputBox}
+                value={password}
+                onChangeText={onChangePassword}
+                placeholder={'password'}
+                keyboardType={'default'}
+                secureTextEntry={true}
+            />
+        </ScrollView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    headerText: {
+        padding: 40,
+        fontSize: 30,
+        color: '#EDEFEE',
+        textAlign: 'center',
+    },
+    regularText: {
+        fontSize: 24,
+        padding: 20,
+        marginVertical: 8,
+        color: '#EDEFEE',
+        textAlign: 'center',
+    },
+    inputBox: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        fontSize: 16,
+        borderColor: '#EDEFEE',
+        backgroundColor: '#EDEFEE',
+    },
+});
